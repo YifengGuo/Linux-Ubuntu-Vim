b9da0856de57f0a2e4bd5b8f34a30b3c321be09a
Yifeng Guo <gyf940517@gmail.com>
2018-03-07 00:45:48 -0500

    cat command practice

---
+++ Ubuntu-Homework/cat cmd/cat_cmd.sh
@@ -0,0 +1,11 @@
+# take a look at test1.txt
+cat test1.txt
+
+# overwrite the content of test1.txt to test2.txt
+cat test1.txt > text2.txt
+# append the content of test1.txt to test2.txt
+cat test1.txt >> text2.txt
+
+# > overwrites the file if it exists or creates it if it doesn't exist
+# >> appends to a file or creates the file if it doesn't exist
+
